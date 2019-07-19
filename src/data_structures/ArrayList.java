e9ae7cbdae3e143747f3a0344d2378e9ef0e16d9
Nnachevvv <nnachevv@icloud.com>
2019-07-19 15:25:23 +0300

    Refactoring , create ArrayList DataStructure

---
+++ src/data_structures/ArrayList.java
@@ -0,0 +1,142 @@
+package data_structures;
+
+import java.lang.reflect.Array;
+/*
+TO DO :
+TEST
+CHECK IF LOGIC IS TRUE
+EQUALLITY COMPRASION
+MAYBE GENERIC SPECIALIZIATION?
+
+ */
+
+
+public class ArrayList {
+
+    private int count;
+    private int capacity;
+    private Object[] elements;
+
+    ArrayList() {
+        this.capacity = 2;
+        this.count = 0;
+        this.elements = new Object[capacity];
+    }
+
+
+    public void add(Object element) {
+        if (count + 1 == capacity) {
+            ensureCapacity(capacity * 2);
+        }
+        elements[count] = element;
+        count += 1;
+    }
+
+    public void add(int index, Object element) {
+        if (count + 1 == capacity) {
+            ensureCapacity(capacity * 2);
+        }
+        for (int i = count; i >= index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        count += 1;
+
+
+    }
+
+    public Object remove(int index) {
+        if (index > count || index < 0 || index >= capacity) {
+            return null;
+        }
+
+        Object removedElement = elements[index];
+        elements[index] = elements[count - 1];
+        --count;
+
+        return removedElement;
+    }
+
+
+    public Object get(int index) {
+        if (index > count || index < 0 || index >= capacity) {
+            return null;
+        }
+        return elements[index];
+    }
+
+
+    public Object set(int index, Object element) {
+        if (index > count || index < 0 || index >= capacity) {
+            return null;
+        }
+        Object tempElement = elements[index];
+        this.elements[index] = element;
+        return tempElement;
+
+    }
+
+    public void clear() {
+        elements = new Object[capacity];
+        count = 0;
+        capacity = 2;
+    }
+
+
+     void ensureCapacity(int minCapacity) {
+        Object[] temp = new Object[minCapacity];
+        System.arraycopy(elements, 0, temp, 0, elements.length);
+        this.elements = temp;
+        this.capacity = minCapacity;
+    }
+
+    public boolean contains(Object element) {
+        for (int i = 0; i < count; i++) {
+            if (element.equals(elements[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(Object element) {
+        for (int i = 0; i < count; i++) {
+            if (elements[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object element) {
+        for (int i = count - 1; i >= 0; i--) {
+            if (elements[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Object[] toArray() {
+        return elements;
+    }
+
+    public void trimToSize() {
+        this.capacity = count;
+    }
+
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+
+    public int size() {
+        return this.count;
+    }
+
+
+}
+
+
+
--- src/sorting_algoritms/Insertion_sort.java
+++ src/sorting_algoritms/Insertion_sort.java
@@ -1,21 +1,20 @@
 package sorting_algoritms;
 
 public class Insertion_sort {
-    public static void insertion_sort(int [] arr)
-    {
-        for (int i = 1; i <arr.length ; i++) {
-            int curr = arr[i] ;
+    public static void insertion_sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
             int temp = i - 1;
-            while(temp >= 0 && curr < arr[temp])
-            {
-                arr[temp+1] = arr[temp];
+            while (temp >= 0 && curr < arr[temp]) {
+                arr[temp + 1] = arr[temp];
                 --temp;
             }
-            arr[temp+1] = curr;
+            arr[temp + 1] = curr;
         }
     }
+
     public static void main(String[] args) {
-        int[] arr = new int[] {2, 4, 2, 6, 7, -1};
+        int[] arr = new int[]{2, 4, 2, 6, 7, -1};
         insertion_sort(arr);
         for (int element : arr) {
             System.out.println(element);
